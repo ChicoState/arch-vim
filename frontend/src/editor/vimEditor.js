12e2e09ad614fde70e934d1d6a437fd0943a5469
Ryan Meline <ryan.c.meline@gmail.com>
2026-02-25 10:43:30 -0800

    Utilized monaco to make basic browser text editor with vim controls

--- frontend/src/editor/vimEditor.js
+++ frontend/src/editor/vimEditor.js
@@ -1,3 +1,60 @@
+import Editor from "@monaco-editor/react";
+import { useRef } from "react";
+import { initVimMode } from "monaco-vim";
+
 export default function VimEditor(){
-	return <div>Hello World</div>;
+	const editorRef = useRef(null);
+	const vimModeRef = useRef(null);
+
+	function handleMount(editor, monaco) {	
+		editorRef.current = editor;
+
+		const statusNode = document.createElement("div");
+		statusNode.style.position = "absolute";
+		statusNode.style.bottom = "0";
+		statusNode.style.right = "10px";
+		statusNode.style.background = "#1e1e1e";
+		statusNode.color = "white";
+		statusNode.padding = "4px 8px";
+		statusNode.style.fontSize = "12px";
+	
+		editor.getDomNode().appendChild(statusNode);
+
+		vimModeRef.current = initVimMode(editor, statusNode);
+
+		editor.onKeyDown((e) => {
+			console.log("Key pressed: ", e.browserEvent.key);
+		});
+	}
+
+	function checkAnswer() {
+		const expectedSolution = `function App() {
+		return <h1> Goodbye React </h1> 
+		}`;
+		const userCode = editorRef.current.getValue();
+		console.log("User code: ", userCode);
+
+		if(userCode.trim() === expectedSolution.trim()) {
+			alert("Correct");
+			//whatever else for correct
+		} else {
+			alert("Nope");
+		}
+	}
+
+	return(
+		<>
+		<Editor
+		height = "500px"
+		width = "1000px"
+		defaultLanguage="javascript" //This is for highlighting
+		defaultValue={`function App() {
+		return <h1> Hello React </h1> 
+		}`} //This is where example code goes
+		onMount={handleMount}
+		/>
+
+		<button onClick={checkAnswer}>Check</button>
+		</>
+	      );
 }
