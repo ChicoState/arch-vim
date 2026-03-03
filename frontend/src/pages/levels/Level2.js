44e06d52e09ea853ecf0739a3a4b52e24f10d6e4
iRainy22 <rainier.ring@gmail.com>
2026-03-02 22:16:13 -0800

    Switch level2 to rely on it's own monaco editor

--- frontend/src/pages/levels/Level2.js
+++ frontend/src/pages/levels/Level2.js
@@ -1,22 +1,95 @@
-
 import { Link } from "react-router-dom";
-import VimEditor from "../../editor/vimEditor";
+import Editor from "@monaco-editor/react";
+import { useRef, useState } from "react";
+import { initVimMode } from "monaco-vim";
 
 export default function Level2() {
-  return (
-    <div style={{ padding: "10px" }}>
-      <h1>Level 2</h1>
-      <h3>Learn how to save and exit a file</h3>
-      <p>
-        When starting with vim its not always clear how to exit<br></br>
-        To open the console, press : <br></br>
-        Typing w will save the file, similar to ctrl+s <br></br>
-        Typing q will exit the vim editer <br></br>
-        These commands can be combined, so typing wq will save and quit the current editor <br></br><br></br>
-        Objective: Make and save a couple changes, then close the editor
-      </p>
 
-      <VimEditor />
+  //Passed Flag
+  const [passed, setPassed] = useState(false);
+  const editorRef = useRef(null);
+	const vimModeRef = useRef(null);
+
+
+  function handleMount(editor, monaco) {	
+		editorRef.current = editor;
+		const editorDom = editor.getDomNode();
+		editorDom.style.position = "relative";
+		
+		//Vim current mode at bottom
+		const statusNode = document.createElement("div");
+		statusNode.style.position = "absolute";
+		statusNode.style.bottom = "0";
+		statusNode.style.right = "50px";
+		statusNode.style.background = "#1e1e1e";
+		statusNode.padding = "4px 8px";
+		statusNode.style.fontSize = "12px";
+	
+		editor.getDomNode().appendChild(statusNode);
+		vimModeRef.current = initVimMode(editor, statusNode);
+
+		//Cursor line info at bottom
+		const cursorPosNode = document.createElement("div");
+		cursorPosNode.style.position = "absolute";
+		cursorPosNode.style.bottom = "0";
+		cursorPosNode.style.left = "35px";
+		cursorPosNode.style.background = "#1e1e1e";
+		cursorPosNode.padding = "4px 8px";
+		cursorPosNode.style.fontSize = "12px";
+
+		editor.getDomNode().appendChild(cursorPosNode);
+		
+		editor.onDidChangeCursorSelection(e => {
+			console.log("Cursor Info: ", e);
+            const line = e.selection.positionLineNumber;
+            const col = e.selection.positionColumn;
+			cursorPosNode.innerText = `Ln ${line}, Col ${col}`;
+            //FOR CHECKING POSITION FOR THIS SPECIFIC LEVEL
+            if(line === 4 && col === 15) setPassed(true);
+		});
+  }
+  //Page Content
+  return (
+    <div class="level2" style={{ padding: "10px" }}>
+      <div class="level_info">
+        <h1>Level 2</h1>
+        <h3>Learn how to exit a file</h3>
+        <p>
+          When starting with vim its not always clear how to exit
+          <br />
+          To open the console, press :
+          <br />
+          Typing ':q' will exit the vim editer
+          <br />
+          If you make an accidental change, and want to quit without saving, type ':q!'
+          <br />
+          <br />
+          Objective: Simply Close the editor
+        </p>
+      </div>
+      <div class="editor">
+        <Editor
+          height = "500px"
+          width = "1000px"
+          options = {{
+            minimap: { enabled: false }
+          }}
+
+          onMount={handleMount}
+          theme = "vs-dark"
+          defaultLanguage="c" //This is for highlighting
+          defaultValue=
+          { //Default code that appears on editor
+            `#include <stdio.h>
+            
+            void main() {
+              printf("Hello World");
+              return 0; 
+            }
+            `
+          }
+        />
+      </div>
     </div>
   );
 }
