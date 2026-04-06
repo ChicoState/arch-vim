4f8ad7751f11514efbf511cbe2b68d55d2432c47
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-06 14:05:03 -0700

    Changed lvl 1 to match new editor implementation

--- frontend/src/pages/levels/Level1.js
+++ frontend/src/pages/levels/Level1.js
@@ -2,61 +2,19 @@ import { Link } from "react-router-dom";
 import Editor from "@monaco-editor/react";
 import { useRef, useState } from "react";
 import { initVimMode } from "monaco-vim";
+import VimEditor from "../../editor/vimEditor";
 
 export default function Level1() {
     //used to make the "You passed!"
     const [passed, setPassed] = useState(false);
+    const startValue =
+`#include <stdio.h>
 
-    const editorRef = useRef(null);
-	const vimModeRef = useRef(null);
-
-	function handleMount(editor, monaco) {	
-		editorRef.current = editor;
-		const editorDom = editor.getDomNode();
-		editorDom.style.position = "relative";
-		
-		//Vim current mode at bottom
-		const statusNode = document.createElement("div");
-		statusNode.style.position = "absolute";
-		statusNode.style.bottom = "0";
-		statusNode.style.right = "50px";
-		statusNode.style.background = "#1e1e1e";
-		statusNode.padding = "4px 8px";
-		statusNode.style.fontSize = "12px";
-	
-		editor.getDomNode().appendChild(statusNode);
-		vimModeRef.current = initVimMode(editor, statusNode);
-
-		//Cursor line info at bottom
-		const cursorPosNode = document.createElement("div");
-		cursorPosNode.style.position = "absolute";
-		cursorPosNode.style.bottom = "0";
-		cursorPosNode.style.left = "35px";
-		cursorPosNode.style.background = "#1e1e1e";
-		cursorPosNode.padding = "4px 8px";
-		cursorPosNode.style.fontSize = "12px";
-
-		editor.getDomNode().appendChild(cursorPosNode);
-		
-		editor.onDidChangeCursorSelection(e => {
-			console.log("Cursor Info: ", e);
-            const line = e.selection.positionLineNumber;
-            const col = e.selection.positionColumn;
-			cursorPosNode.innerText = `Ln ${line}, Col ${col}`;
-            //FOR CHECKING POSITION FOR THIS SPECIFIC LEVEL
-            if(line === 4 && col === 15) setPassed(true);
-		});
-	
-		//Key logger (use for checking for certain key presses)
-		editor.onKeyDown((e) => {
-            const key = e.browserEvent.key;
-			console.log("Key pressed: ", key);
-            if(key === "ArrowUp" || key === "ArrowDown" || key === "ArrowLeft" || key === "ArrowRight") {
-                alert("Please don't use arrow keys");
-            }
-		});
-	}
-
+int main() {
+	printf("Hello World");
+	return 0; 
+}
+`
     return (
 
 // PAGE CONTENTS
@@ -73,28 +31,13 @@ export default function Level1() {
 
 {/* EDITOR IMPLEMENTATION */}
         <>
-		<Editor
-		height = "500px"
-		width = "1000px"
-        options = {{
-			minimap: { enabled: false }
-		}}
-		onMount={handleMount}
-		theme = "vs-dark"
-		defaultLanguage="c" //This is for highlighting
-		defaultValue=
-{ //Default code that appears on editor
-`#include <stdio.h>
-
-int main() {
-	printf("Hello World");
-	return 0; 
-}
-`
-}
+        <VimEditor
+        value = {startValue}
+        cursorCol={15}
+        cursorLine={4}
+        onWin = {() => passed(true)}
         />
-        {/* Check button. Thinking about changing this to an automatic thing that fires when the cursor gets to those spots, rather than waiting to press the check button */}
-		{passed && (
+        {passed && (
             <div style={{
                 marginTop: "20px",
                 padding: "10px",
