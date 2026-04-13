7dc0108aff2df5313f84315d0d1cfdc817324839
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-13 00:24:37 -0700

    Removed Nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,16 @@ import Level2 from './pages/levels/Level2.js';
 import Level3 from './pages/levels/Level3.js';
 import Level4 from './pages/levels/Level4.js';
 import Level5 from './pages/levels/Level5.js';
+import Level6 from './pages/levels/Level6.js';
+import Level7 from './pages/levels/Level7.js';
+import Level8 from './pages/levels/Level8.js';
+import Level9 from './pages/levels/Level9.js';
+import Level10 from './pages/levels/Level10.js';
+import Level11 from './pages/levels/Level11.js';
+import Level12 from './pages/levels/Level12.js';
+import Level13 from './pages/levels/Level13.js';
+import Level14 from './pages/levels/Level14.js';
+import Level15 from './pages/levels/Level15.js';
 import LevelTest from './pages/levels/levelTest.js';
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
@@ -41,9 +51,10 @@ function App() {
       .catch((err) => console.error(err));
   }, []);
   return (
+    <>
     <AuthProvider>
       <BrowserRouter>
-        <Nav />
+        {/* <Nav /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
@@ -54,10 +65,21 @@ function App() {
           <Route path="/levels/3" element={<Level3 />} />
           <Route path="/levels/4" element={<Level4 />} />
           <Route path="/levels/5" element={<Level5 />} />
+          <Route path="/levels/6" element={<Level6 />} />
+          <Route path="/levels/7" element={<Level7 />} />
+          <Route path="/levels/8" element={<Level8 />} />
+          <Route path="/levels/9" element={<Level9 />} />
+          <Route path="/levels/10" element={<Level10 />} />
+          <Route path="/levels/11" element={<Level11 />} />
+          <Route path="/levels/12" element={<Level12 />} />
+          <Route path="/levels/13" element={<Level13 />} />
+          <Route path="/levels/14" element={<Level14 />} />
+          <Route path="/levels/15" element={<Level15 />} />
           <Route path="/levels/test" element={<LevelTest/>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
+    </>
 //ignore this, this was the default react stuff, I'm keeping as a reference for formatting later
   //   <div className="App">
   //     <header className="App-header">
