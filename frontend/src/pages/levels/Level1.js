80da1ab7f3c7562c760b4312647a0bd2e2e6245e
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-13 00:28:15 -0700

    Removed include link

--- frontend/src/pages/levels/Level1.js
+++ frontend/src/pages/levels/Level1.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import VimEditor from "../../editor/vimEditor";
 import { loadProgress } from "../../progress";
