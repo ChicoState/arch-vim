fe4d00b6b1195231f4d38e23eb8375766baecb17
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-12 18:27:52 -0700

    Added UI to Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -21,27 +21,31 @@ function Login() {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={e => setUsername(e.target.value)}
-                />
-                <br></br>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-                <br></br>
-                <button type="submit">Login</button>
-            </form>
-            <p>Don't have an account? <Link to="/register">Sign Up Here</Link></p>
+        <div className="bg-gray-950">
+            <div className="min-h-screen text-gray-200 flex flex-col justify-center items-center">
+                <h1 className="text-center text-4xl mb-4">Login</h1>
+                <div className="bg-gray-950 rounded-2xl p-6 text-gray-200 shadow-[0_0_5px_rgba(99,102,241,0.7)]">
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-2">
+                        <input
+                            className="w-35 h-12 bg-gray-950 rounded-2xl p-6 text-gray-200 shadow-[0_0_5px_rgba(99,102,241,0.7)]"
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                        <input
+                            className="w-35 h-12 bg-gray-950 rounded-2xl p-6 text-gray-200 shadow-[0_0_5px_rgba(99,102,241,0.7)]"
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                        <button type="submit" className="self-center pl-4 pr-4 pt-1 pb-1 rounded-xl text-centere bg-[rgb(99,102,241)]/50 text-center">Login</button>
+                    </form>
+                    <p className="text-xs text-gray-400 text-center">Don't have an account? <Link to="/register" className="text-blue-400">Sign Up Here</Link></p>
+                </div>
+            </div>
         </div>
     );
 }
