08c52d813b92aa6b58560d0d2a5a341e45367612
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-13 00:26:26 -0700

    Added Levels to sidebar

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import useCheckLevel from "../components/checkLevelPassed";
 import Login from "./login";
 
+function callBackend() {
+    
+}
+
 function LevelCheck({ levelNum=0, levelDesc="" }) {
   const passed = useCheckLevel(levelNum);
   return passed ? (
@@ -17,15 +21,33 @@ export default function Sidebar() {
         <div className="w-[15vw] h-[96vh] rounded-2xl bg-gray-950 shadow-[0_0_20px_rgba(99,102,241,0.7)] p-4 relative text-xl">
             <h1 className="text-center text-2xl mb-2">Navigation</h1>
             <hr className="mb-4 border-gray-600"></hr>
-            <Link to="/" className="mb-4">Home</Link>
+            <Link to="/" className="mb-4 text-xl">Home</Link>
             <h2>Levels</h2>
             <h3 className="pl-4">Easy</h3>
-            <div className="pl-8">
+            <div className="pl-8 mb-2">
                 <LevelCheck levelNum={1} levelDesc={"Learn Navigation"}/><br></br>
                 <LevelCheck levelNum={2} levelDesc={"How to exit a vim file"}/><br></br>
                 <LevelCheck levelNum={3} levelDesc={"Insert Mode and typing"}/><br></br>
                 <LevelCheck levelNum={4} levelDesc={"How to save files"}/><br></br>
                 <LevelCheck levelNum={5} levelDesc={"Challenge!"}/><br></br>
+            </div>
+            <br></br>
+            <h3 className="pl-4">Easy</h3>
+            <div className="pl-8 mb-2">
+                <LevelCheck levelNum={6} levelDesc={"Learn Navigation"}/><br></br>
+                <LevelCheck levelNum={7} levelDesc={"How to exit a vim file"}/><br></br>
+                <LevelCheck levelNum={8} levelDesc={"Insert Mode and typing"}/><br></br>
+                <LevelCheck levelNum={9} levelDesc={"How to save files"}/><br></br>
+                <LevelCheck levelNum={10} levelDesc={"Challenge!"}/><br></br>
+            </div>
+            <br></br>
+            <h3 className="pl-4">Easy</h3>
+            <div className="pl-8">
+                <LevelCheck levelNum={11} levelDesc={"Learn Navigation"}/><br></br>
+                <LevelCheck levelNum={12} levelDesc={"How to exit a vim file"}/><br></br>
+                <LevelCheck levelNum={13} levelDesc={"Insert Mode and typing"}/><br></br>
+                <LevelCheck levelNum={14} levelDesc={"How to save files"}/><br></br>
+                <LevelCheck levelNum={15} levelDesc={"Challenge!"}/><br></br>
             </div>
             <div className="absolute left-5 bottom-5">
                 <Login />
